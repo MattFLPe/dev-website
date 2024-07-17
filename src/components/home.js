bf613845b7ac85de974c69c5961b67b1a06dbdff
Mateus Felipe <brasil.mateus748@gmail.com>
2024-07-17 01:03:32 -0300

    Create new file Add layout

---
+++ src/components/home.js
@@ -0,0 +1,22 @@
+import NavigationBar from './navbar.js';
+import '../App.css';
+
+
+function Home() {
+    return (
+        <div className="container-home">
+            <div class="terminal">
+                <pre><p>ls -l</p></pre>
+                <pre><p>Hello</p></pre>
+                <pre><p>My name is Mateus</p></pre>
+                <pre><p>I'm a web developer</p><span>&nbsp;</span></pre>
+                <pre><p></p></pre>
+                <pre><p></p></pre>
+                <pre><p></p></pre>
+            </div>
+
+        </div>
+    )
+}
+
+export default Home;

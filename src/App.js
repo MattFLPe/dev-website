b00c99f53a990b2c9695a334e1d0b29504c0aec8
Mateus Felipe <brasil.mateus748@gmail.com>
2024-11-01 23:56:11 -0300

    Added two routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './components/home.css'
+import './components/navbar.css';
+import NavigationBar from './components/navbar';
+import Home from './components/home.js'
+import Resume from './components/resume.js';
 
 function App() {
   return (
-    <div className="App">
-     <header>Site</header>
-    </div>
+    <Router>
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/components/resume.js" element={<Resume />} />
+      </Routes>
+    </Router>
   );
 }
 
